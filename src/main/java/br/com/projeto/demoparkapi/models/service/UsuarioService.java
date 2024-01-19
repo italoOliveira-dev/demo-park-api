9b0f4f7e16cf75c9bc5318d24f11e4bad1fcf872
Italo Oliveira <italooliveira-dev@hotmail.com>
2024-01-18 22:24:48 -0300

    feat: feature for create user

--- src/main/java/br/com/projeto/demoparkapi/models/service/UsuarioService.java
+++ src/main/java/br/com/projeto/demoparkapi/models/service/UsuarioService.java
@@ -1,8 +1,21 @@
 package br.com.projeto.demoparkapi.models.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.projeto.demoparkapi.models.entity.Usuario;
+import br.com.projeto.demoparkapi.models.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
     
 }
--- src/main/java/br/com/projeto/demoparkapi/web/controller/UsuarioController.java
+++ src/main/java/br/com/projeto/demoparkapi/web/controller/UsuarioController.java
@@ -1,10 +1,27 @@
 package br.com.projeto.demoparkapi.web.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projeto.demoparkapi.models.entity.Usuario;
+import br.com.projeto.demoparkapi.models.service.UsuarioService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("api/v1/usuarios")
 public class UsuarioController {
     
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<Usuario> criar(@RequestBody Usuario usuario) {
+        Usuario user = usuarioService.salvar(usuario);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+    }
 }
